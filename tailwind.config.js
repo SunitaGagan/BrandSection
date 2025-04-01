3a7ac9b376a3ce5ef04c43872ec94b6f4314d5ea
yadavsunita <sunita.sun15april@gmail.com>
2025-04-01 12:01:53 +0530

    add brand section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./publice/*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ["Inter", "sans-serif"],
+            },
+
+        },
+    },
+    plugins: [],
+}
